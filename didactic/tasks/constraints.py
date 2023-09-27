7f94996036bff75c1f335a6752812af05fd3287f
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2023-09-27 15:48:27 +0200

    Add impl. of neighborhood consistency (in encoder's feature space) constraint
    - Include configuration options for `length_scale` based on various estimation methods/registry model versions
    - Configs for different methods of enforcing neighborhood consistency:
    * l2-norm
    * graph alignment
    Improve description of `length_scale` values in neighborhood consistency constraint config

--- didactic/tasks/constraints.py
+++ didactic/tasks/constraints.py
@@ -125,3 +125,83 @@ class ClustersConsistencyConstraint(DistanceBasedConstraint):
             self._dist2clusters_reg_loss(patient_dist2clusters, self._dist2clusters[id].to(out_features.device))
             for id, patient_dist2clusters in zip(item_ids, batch_dist2clusters)
         ) / len(item_ids)
+
+
+class NeighborhoodConsistencyConstraint(DistanceBasedConstraint, ABC):
+    """Constraint that enforces the points to maintain the starting neighborhoods, i.e. affinity, to each other.
+
+    The constraint itself is a comparison between a reference similarity matrix, and a moving similarity matrix, updated
+    by the features predicted for each new batch.
+    i) The pairwise similarity between points is configurable, but a standard choice would be the RBF kernel.
+    ii) The loss term for the comparison of similarity matrices is configurable.
+    """
+
+    def __init__(
+        self, affinity_matrices_loss: Callable[[Tensor, Tensor], Tensor], affinity_eps: float = 1e-3, **kwargs
+    ):
+        """Initializes class instance.
+
+        Args:
+            affinity_matrices_loss: Loss term that compares input and target (D, D) square affinity matrices between
+                items' encodings.
+            affinity_eps: If not `None`, affinity values under this threshold will be clipped to 0, to reduce the effect
+                of distant points and outliers.
+            **kwargs: Additional parameters to pass along to ``super().__init__()``.
+        """
+        super().__init__(**kwargs)
+        self.affinity_matrices_loss = affinity_matrices_loss
+        self.affinity_eps = affinity_eps
+
+    def setup(self, trainer: "pl.Trainer", pl_module: pl.LightningModule, stage: Optional[str] = None) -> None:
+        """Memorizes the pairwise distances between items in the current stage's data."""
+        # For each subset of patients in the data
+        datamodule = trainer.datamodule
+        patient_ids, patient_encodings = [], []
+        for subset_patients in datamodule.subsets_patients.values():
+            # Run inference on the patients included in the data to get a "starting" encoding for each patient
+            subset_encodings = torch.from_numpy(
+                encode_patients(
+                    pl_module, subset_patients.values(), mask_tag=datamodule._process_patient_kwargs.get("mask_tag")
+                )
+            )
+
+            patient_ids.extend(list(subset_patients))
+            patient_encodings.append(subset_encodings)
+
+        self.item_ids = patient_ids
+        ref_item_encodings = torch.vstack(patient_encodings)
+        self.moving_item_encodings = ref_item_encodings.clone()
+
+        self.ref_similarity_matrix = self.cdist(ref_item_encodings, ref_item_encodings)
+        if self.affinity_eps:
+            # Clip small similarities to 0 to avoid too much noise from distant points and outliers
+            self.ref_similarity_matrix = torch.where(
+                self.ref_similarity_matrix > self.affinity_eps, self.ref_similarity_matrix, 0
+            )
+
+    def forward(self, item_ids: Sequence[str], out_features: Tensor) -> Tensor:
+        """Computes a regularization term between distances computed on `out_features` and those memorized.
+
+        Args:
+            item_ids: (N,), IDs uniquely identifying each item in the batch.
+            out_features: (N, E), Features extracted by the model, for each item the batch.
+
+        Returns:
+            (N,), Regularization term, for each item in the batch.
+        """
+        # Detach moving items' encodings to make sure the graph of previous steps is cleared
+        # (so that the backward pass won't try to backprop through previous steps)
+        self.moving_item_encodings = self.moving_item_encodings.detach()
+
+        # Update the moving items' encodings
+        for item_id, item_features in zip(item_ids, out_features):
+            self.moving_item_encodings[self.item_ids.index(item_id)] = item_features
+
+        # Compute the similarity between the updated items' encodings
+        similarity_matrix = self.cdist(self.moving_item_encodings, self.moving_item_encodings)
+        if self.affinity_eps:
+            # Clip small similarities to 0 to avoid too much noise from distant points and outliers
+            similarity_matrix = torch.where(similarity_matrix > self.affinity_eps, similarity_matrix, 0)
+
+        # Compute the loss between the moving and reference similarity matrices
+        return self.affinity_matrices_loss(similarity_matrix, self.ref_similarity_matrix)
