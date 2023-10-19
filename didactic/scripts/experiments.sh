92ebb48188beffc21f774885586b7e69795e5fa7
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2023-10-19 22:55:02 +0200

    Add bash commands to produce embeddings plots + update logging naming scheme

--- didactic/scripts/experiments.sh
+++ didactic/scripts/experiments.sh
@@ -86,6 +86,22 @@ env COMET_PROJECT_NAME=didactic-multimodal-xformer-finetune didactic-runner -m h
 #env COMET_PROJECT_NAME=didactic-multimodal-xformer-finetune didactic-runner -m hydra/launcher=joblib hydra.launcher.n_jobs=10 +experiment=cardinal/cardiac-multimodal-representation-finetune trainer.enable_progress_bar=False task.contrastive_loss_weight=0,0.1 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' excluded_clinical_attrs=[ht_severity,ht_grade] 'ckpt=/home/local/USHERBROOKE/pain5474/data/didactic/models/multimodal-xformer/pretrain/all/multimodal-xformer-pretrain-all-ht_severity-${trial}.ckpt' '+trial=range(10)'
 
 
+# Plot 2D embeddings of the transformer encoder representations
+for model in multimodal-xformer
+for task in scratch scratch-cotrain finetune finetune-cotrain
+for data in top-13 top-13+img all-clin all
+for target in nt_probnp_group ht_severity
+set method_path $model/$task/$data
+set method_name $model-$task-$data-$target
+for model_id in (seq 0 9)
+echo "Generating 2D embedding of latent space of $method_name-$model_id model using PaCMAP" >>$HOME/data/didactic/results/2d_embeddings_$model.log 2>&1
+python didactic/scripts/cardiac_multimodal_representation_plot.py $HOME/data/didactic/models/$method_path/$method_name-$model_id.ckpt --data_roots $HOME/dataset/cardinal/v4/data --views A4C A2C --plot_categorical_attrs_dirs $HOME/dataset/cardinal/v3/patients_by_attr_label/subset --output_dir=$HOME/data/didactic/results/2d_embeddings/$method_path/$target/$model_id >>$HOME/data/didactic/results/2d_embeddings_$model.log 2>&1
+end
+end
+end
+end
+end
+
 # Cluster the transformer encoder representations
 for model in multimodal-xformer
 for task in scratch scratch-cotrain finetune finetune-cotrain
@@ -104,7 +120,6 @@ end
 end
 end
 
-
 # Running clustering evaluation of transformer encoder representations
 for model in multimodal-xformer
 for task in scratch scratch-cotrain finetune finetune-cotrain
@@ -112,8 +127,8 @@ for data in top-13 top-13+img all-clin all
 for target in nt_probnp_group ht_severity
 set method_path $model/$task/$data/$target
 set method_name $model-$task-$data-$target
-echo "Evaluating GMM clustering of $method_name models" >>$HOME/data/didactic/results/clustering_eval.log 2>&1
-python didactic/scripts/describe_representation_clustering.py (ls $HOME/data/didactic/results/clustering/$method_path/**/predictions.csv) --data_roots=$HOME/dataset/cardinal/v4/data --views A4C A2C --output_dir=$HOME/data/didactic/results/clustering_eval/$method_path >>$HOME/data/didactic/results/clustering_eval.log 2>&1
+echo "Evaluating GMM clustering of $method_name models" >>$HOME/data/didactic/results/clustering_eval_$model.log 2>&1
+python didactic/scripts/describe_representation_clustering.py (ls $HOME/data/didactic/results/clustering/$method_path/**/predictions.csv) --data_roots=$HOME/dataset/cardinal/v4/data --views A4C A2C --output_dir=$HOME/data/didactic/results/clustering_eval/$method_path >>$HOME/data/didactic/results/clustering_eval_$model.log 2>&1
 end
 end
 end
@@ -127,8 +142,8 @@ for target in nt_probnp_group ht_severity
 for ref_attr in ht_severity ht_grade
 set method_path $model/$task/$data
 set method_name $model-$task-$data-$target
-echo "Evaluating KNN representations of $method_name models" >>$HOME/data/didactic/results/knn_eval.log 2>&1
-python didactic/scripts/describe_representation_knn.py (ls $HOME/data/didactic/models/$method_path/*$target*.ckpt) --data_roots $HOME/dataset/cardinal/v4/data --views A4C A2C --output_dir=$HOME/data/didactic/results/knn_eval/$method_path/$target '--neigh_kwargs={n_neighbors:8}' '--clinical_plot_kwargs={color:model}' --image_n_bins=8 --reference_attr=$ref_attr >>$HOME/data/didactic/results/knn_eval.log 2>&1
+echo "Evaluating KNN representations of $method_name models" >>$HOME/data/didactic/results/knn_eval_$model.log 2>&1
+python didactic/scripts/describe_representation_knn.py (ls $HOME/data/didactic/models/$method_path/*$target*.ckpt) --data_roots $HOME/dataset/cardinal/v4/data --views A4C A2C --output_dir=$HOME/data/didactic/results/knn_eval/$method_path/$target '--neigh_kwargs={n_neighbors:8}' '--clinical_plot_kwargs={color:model}' --image_n_bins=8 --reference_attr=$ref_attr >>$HOME/data/didactic/results/knn_eval_$model.log 2>&1
 end
 end
 end
