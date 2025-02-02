96121945de57fe195654bc4709a4a26b51cce99f
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-02-02 19:29:41 +0100

    Fix various issues with eval shell script
    - Fix env var
    - Copy dataset partition files into "subset" sub-folders to facilitate automatically defining legend titles
    - Fix paths to folders listing patients by bins
    - Add args to style plot markers w.r.t. dataset partitioning

--- shell_scripts/eval.sh
+++ shell_scripts/eval.sh
@@ -16,20 +16,24 @@ python ~/remote/didactic/didactic/scripts/plot_models_variability.py "$MODEL_COM
 # For each of the model
 for split_idx in $(seq 0 4); do
 
+  # Copy the dataset partitioning files to "Subset" sub-directories (for plotting purposes)
+  mkdir -p $OUTPUT_DIR/split_idx=$split_idx/Subset
+  rsync -av $CARDINAL_DATA_PATH/splits/$split_idx/ $OUTPUT_DIR/split_idx=$split_idx/Subset
+
   # Split patients into bins w.r.t. continuum param predicted for each patient
   echo "Splitting patients into bins w.r.t. continuum param for model on split $split_idx" >>$OUTPUT_DIR/group_patients_by_continuum_param.log 2>&1
-  python ~/remote/didactic/didactic/scripts/group_patients_by_predictions.py "$MODEL_COMMON_PATH-$split_idx" --data_roots CARDINAL_DATA_PATH --views A4C A2C --bins=6 --output_dir=$OUTPUT_DIR/split_idx=$split_idx/continuum_param_bins range --bounds 0 1 >>$OUTPUT_DIR/group_patients_by_continuum_param.log 2>&1
+  python ~/remote/didactic/didactic/scripts/group_patients_by_predictions.py "$MODEL_COMMON_PATH-$split_idx" --data_roots $CARDINAL_DATA_PATH --views A4C A2C --bins=6 --output_dir=$OUTPUT_DIR/split_idx=$split_idx/continuum_param_bins range --bounds 0 1 >>$OUTPUT_DIR/group_patients_by_continuum_param.log 2>&1
 
   # Describe the tabular attributes of the patients in each bin
   echo "Describing tabular attributes of patients in each bin for model on split $split_idx" >>$OUTPUT_DIR/describe_patients_by_bins.log 2>&1
-  python ~/remote/didactic/didactic/scripts/describe_patients.py --data_roots $CARDINAL_DATA_PATH --views A4C A2C --attributes mean_e_prime la_volume lvm_ind --subsets $(find $OUTPUT_DIR/continuum_param_bins/split_idx=$split_idx -name "*.txt" | sort | tr "\n" " ") --output_dir=$OUTPUT_DIR/split_idx=$split_idx/describe_patients_by_bins >>$OUTPUT_DIR/describe_patients_by_bins.log 2>&1
+  python ~/remote/didactic/didactic/scripts/describe_patients.py --data_roots $CARDINAL_DATA_PATH --views A4C A2C --subsets $(find $OUTPUT_DIR/split_idx=$split_idx/continuum_param_bins -name "*.txt" | sort | tr "\n" " ") --output_dir=$OUTPUT_DIR/split_idx=$split_idx/describe_patients_by_bins >>$OUTPUT_DIR/describe_patients_by_bins.log 2>&1
 
   # Plot variability of time-series attributes w.r.t. predicted continuum
   echo "Plotting variability of attrs w.r.t. continuum predicted by model on split $split_idx" >>$OUTPUT_DIR/attrs_wrt_continuum_bins.log 2>&1
-  python ~/remote/didactic/vital/vital/data/cardinal/plot_attrs_wrt_groups.py --data_roots $CARDINAL_DATA_PATH --views A4C A2C --groups_txt $(find $OUTPUT_DIR/continuum_param_bins/split_idx=$split_idx -name "*.txt" | sort | tr "\n" " ") '--time_series_plot_kwargs={errorbar:,palette:flare}' --output_dir=$OUTPUT_DIR/split_idx=$split_idx/attrs_wrt_continuum_param >>$OUTPUT_DIR/attrs_wrt_continuum_param.log 2>&1
+  python ~/remote/didactic/vital/vital/data/cardinal/plot_attrs_wrt_groups.py --data_roots $CARDINAL_DATA_PATH --views A4C A2C --groups_txt $(find $OUTPUT_DIR/split_idx=$split_idx/continuum_param_bins -name "*.txt" | sort | tr "\n" " ") '--time_series_plot_kwargs={errorbar:,palette:flare}' --output_dir=$OUTPUT_DIR/split_idx=$split_idx/attrs_wrt_continuum_param >>$OUTPUT_DIR/attrs_wrt_continuum_param.log 2>&1
 
   # Plot 2D embeddings of the transformer encoder representations
   echo "Generating 2D embedding of latent space for model on split $split_idx, using PaCMAP" >>$OUTPUT_DIR/2d_embeddings.log 2>&1
-  python ~/remote/didactic/didactic/scripts/cardiac_multimodal_representation_plot.py "$MODEL_COMMON_PATH-$split_idx" --data_roots $CARDINAL_DATA_PATH --views A4C A2C --plot_categorical_attrs_dirs $OUTPUT_DIR/split_idx=$split_idx/continuum_param_bins --plot_markers_attrs_dir=$CARDINAL_DATA_PATH/splits/$split_idx '--num_plot_kwargs={palette:flare}' --output_dir=$OUTPUT_DIR/split_idx=$split_idx/2d_embeddings >>$OUTPUT_DIR/2d_embeddings.log 2>&1
+  python ~/remote/didactic/didactic/scripts/cardiac_multimodal_representation_plot.py "$MODEL_COMMON_PATH-$split_idx" --data_roots $CARDINAL_DATA_PATH --views A4C A2C --plot_categorical_attrs_dirs $OUTPUT_DIR/split_idx=$split_idx/continuum_param_bins $OUTPUT_DIR/split_idx=$split_idx/Subset '--cat_plot_kwargs={style:Subset}' '--num_plot_kwargs={style:Subset,palette:flare}' --output_dir=$OUTPUT_DIR/split_idx=$split_idx/2d_embeddings >>$OUTPUT_DIR/2d_embeddings.log 2>&1
 
 done
